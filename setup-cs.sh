fe956538f5d5b7fd60cf274621207e34f37f0a81
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-02-20 17:47:39 +0100

    fixup build

---
+++ setup-cs.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION="2.0.0-RC6-10"
+
+URL=""
+CACHE_BASE=""
+CS="./cs"
+DO_CHMOD="1"
+
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux/17072017#17072017
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  URL="https://github.com/coursier/coursier/releases/download/v$VERSION/cs-x86_64-pc-linux"
+  CACHE_BASE="$HOME/.cache/coursier/v1"
+elif [ "$(uname)" == "Darwin" ]; then
+  URL="https://github.com/coursier/coursier/releases/download/v$VERSION/cs-x86_64-apple-darwin"
+  CACHE_BASE="$HOME/Library/Caches/Coursier/v1"
+else
+  # assuming Windows…
+  URL="https://github.com/coursier/coursier/releases/download/v$VERSION/cs-x86_64-pc-win32.exe"
+  env
+  CACHE_BASE="$LocalAppData/Coursier/v1" # TODO Check that
+  CS="cs.exe"
+DO_CHMOD="0"
+fi
+
+DEST="$CACHE_BASE/$(echo "$URL" | sed 's@://@/@')"
+
+if [ -f "$DEST" ]; then
+  echo "Found $DEST in cache"
+else
+  mkdir -p "$(dirname "$DEST")"
+  TMP_DEST="$DEST.tmp-setup"
+  echo "Downloading $URL"
+  curl -Lo "$TMP_DEST" "$URL"
+  mv "$TMP_DEST" "$DEST"
+fi
+
+cp "$DEST" "$CS"
+if [ "$DO_CHMOD" = "1" ]; then
+  chmod +x "$CS"
+fi
+
+"$CS" --help
