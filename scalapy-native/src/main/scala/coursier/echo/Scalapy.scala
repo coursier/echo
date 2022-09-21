deb559dcc7a4970b8d01262f758651b62b5327b5
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2022-09-21 15:44:48 +0200

    Flush things when printing with Python's print method (#79)

--- scalapy-native/src/main/scala/coursier/echo/Scalapy.scala
+++ scalapy-native/src/main/scala/coursier/echo/Scalapy.scala
@@ -8,14 +8,10 @@ object Scalapy {
   def main(args: Array[String]): Unit = {
     py.local {
       val args0 = args.toSeq.map(x => x: py.Any)
-      // What we really want to do is:
+      // Getting "applyDynamic does not support passing a vararg parameter" with
       //   py.Dynamic.global.print(args0: _*)
-      // but getting "applyDynamic does not support passing a vararg parameter"
-      CPythonInterpreter.callGlobal(
-        "print",
-        args0.map(Helper.scalapyValue),
-        Seq()
-      )
+      // Using @kiendang's suggestion instead (https://gitter.im/shadaj/scalapy?at=6324a9ef72ad51741fe3afb5)
+      py.Dynamic.global.applyDynamicNamed("print")(args0.map("" -> _) ++ Seq("flush" -> py.Any.from(true)): _*)
 
       ()
     }
